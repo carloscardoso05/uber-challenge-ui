62df940e2beb02380ea373503b9931bb19a944ae
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2025-02-27 12:08:54 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="h-screen w-screen flex flex-col">
+    <header class="flex w-full justify-end">
+      <Button variant="outline" class="m-2" size="icon" :onclick="() => toggleDark()">
+        <Icon icon="solar:moon-linear" class="hidden dark:block"></Icon>
+        <Icon icon="material-symbols:sunny-outline-rounded" class="dark:hidden"></Icon>
+      </Button>
+    </header>
+    <Card class="max-w-xl m-auto">
+      <CardHeader>
+        <CardTitle class="text-4xl">Email Sender</CardTitle>
+        <CardDescription>
+          Send a email using our email service that abstracts the use of AWS SES and Twillio
+          SendGrid
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div>
+          <Label for="email-input">Receiver email</Label>
+          <Input type="email" id="email-input" placeholder="example@domain.com" />
+        </div>
+        <div>
+          <Label for="subject-input">Subject</Label>
+          <Input type="text" id="subject-input" placeholder="Enter email subject" />
+        </div>
+        <div>
+          <Label for="message-input">Message</Label>
+          <Textarea id="message-input" placeholder="Type your message here..." />
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { Icon } from '@iconify/vue'
+import { useDark, useToggle } from '@vueuse/core'
+import Button from './components/ui/button/Button.vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import autoprefixer from 'autoprefixer'
+import { fileURLToPath, URL } from 'node:url'
+import tailwind from 'tailwindcss'
+
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+})
