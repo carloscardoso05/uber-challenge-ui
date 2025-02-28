7fe19de751e9644b6dbbaf7fd0b8b7c3fda1439d
Carlos Silva <cvcardos5@gmail.com>
2025-02-27 22:49:08 -0300

    adjust validation in emailSchema

--- src/models/email.model.ts
+++ src/models/email.model.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 
 export const emailSchema = z.object({
   receiver: z.string().email(),
-  subject: z.string().default(''),
-  body: z.string().default(''),
+  subject: z.string().nonempty(),
+  body: z.string().nonempty(),
 })
 
 export type Email = z.infer<typeof emailSchema>
